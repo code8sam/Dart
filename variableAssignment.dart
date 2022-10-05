5402deadb902eb3d021844be424ff6be0b6ef000
code8sam <work.social.satyam@gmail.com>
2022-10-06 04:20:02 +0530

    Variable Assignment

---
+++ variableAssignment.dart
@@ -0,0 +1,11 @@
+void main() {
+  // String decalre and initialization
+  String sentence = 'Dart is the new way';
+  // print
+  print('$sentence');
+  // assignment of a variable
+  // giving new value to the existing variable :
+  sentence = sentence.toUpperCase();
+  // print
+  print('$sentence');
+}
