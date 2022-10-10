22c5d1688c2864cd2f98353c6ccb20e24b0418ca
code8sam <work.social.satyam@gmail.com>
2022-10-10 18:49:53 +0530

    Datatypes in Dart

---
+++ datatypes.dart
@@ -0,0 +1,12 @@
+void main() {
+  // remember Dart is case sensitive language.
+  String firstName = "Satyam";
+  int age = 23;
+  double height = 5.8;
+  bool likesDart = true;
+  if (likesDart) {
+    print(
+        "$firstName who is $age years old and is ${height}ft tall likes dart language.");
+  }
+  // output: Satyam who is 23 years old and is 5.8ft tall likes dart language.
+}
