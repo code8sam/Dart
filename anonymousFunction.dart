e191e45f746d5e6cdbc764c3afa85117694aa772
code8sam <work.social.satyam@gmail.com>
2023-09-11 09:59:28 +0530

    anonymous function in dart

---
+++ anonymousFunction.dart
@@ -0,0 +1,9 @@
+void main() {
+  print(sum(3, 5));
+}
+
+// anonymous function here ...
+// Function sum = (int a, int b) => a + b;
+
+// Argument types can be omitted in anonymous function. It’s a common thing.
+Function sum = (a, b) => a + b;
